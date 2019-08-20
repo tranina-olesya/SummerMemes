64636d45d8208345624625731c42f9b6de9e2789
Olesya Tranina <tranina.o@gmail.com>
2019-08-20 19:42:09 +0300

    UI для SplashActivity

--- app/src/main/java/ru/vsu/summermemes/ui/authorization/AuthorizationActivity.kt
+++ app/src/main/java/ru/vsu/summermemes/ui/authorization/AuthorizationActivity.kt
@@ -1,12 +1,13 @@
-package ru.vsu.summermemes
+package ru.vsu.summermemes.ui.authorization
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ru.vsu.summermemes.R
 
-class MainActivity : AppCompatActivity() {
+class AuthorizationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_authorization)
     }
 }
---
+++ app/src/main/java/ru/vsu/summermemes/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package ru.vsu.summermemes.ui.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.vsu.summermemes.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
