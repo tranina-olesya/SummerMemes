32c2882f303b7a305ca08b0d371bcea42502079c
Olesya Tranina <tranina.o@gmail.com>
2019-08-21 16:41:03 +0300

    Запрос для авторизации пользователя

--- app/src/main/java/ru/vsu/summermemes/models/UserInfo.kt
+++ app/src/main/java/ru/vsu/summermemes/models/UserInfo.kt
@@ -1,3 +1,9 @@
 package ru.vsu.summermemes.models
 
-data class UserInfo(val id: Long, val username: String, val firstName: String, val lastName: String, val userDescription: String)
+data class UserInfo(
+    val id: Long,
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    val userDescription: String
+)
